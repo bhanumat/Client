6a4d1449601ef2762c6a3e67866f8d5fd8b2756b
bhanumat <bhanumat.w@gmail.com>
2016-06-26 18:10:55 +0700

    Introduce Abstract Element Locator

--- Client/src/test/java/com/bhanumat/client/SignupPage.java
+++ Client/src/test/java/com/bhanumat/client/SignupPage.java
@@ -9,6 +9,13 @@ public class SignupPage {
 	WebDriver driver;
 	WebDriverWait wait;
 
+	By firstNameTextBoxLocator = By.id("user_first_name");
+	By lastNameTextBoxLocator = By.id("user_last_name");
+	By emailTextBoxLocator = By.id("user_email");
+	By passwordTextBoxLocator = By.id("user_password");
+	By confirmPasswordTextBoxLocator = By.id("user_password_confirmation");
+	By signupButtonLocator = By.name("commit");
+
 	public SignupPage(WebDriver driver) {
 		this.driver = driver;
 		wait = new WebDriverWait(driver, 2000);
@@ -16,26 +23,26 @@ public class SignupPage {
 	}
 
 	public void she_provides_the_first_name_as(String firstName) throws Throwable {
-		driver.findElement(By.id("user_first_name")).sendKeys(firstName);
+		driver.findElement(firstNameTextBoxLocator).sendKeys(firstName);
 	}
 
 	public void she_provides_the_last_name_as(String lastName) throws Throwable {
-		driver.findElement(By.id("user_last_name")).sendKeys(lastName);
+		driver.findElement(lastNameTextBoxLocator).sendKeys(lastName);
 	}
 
 	public void she_provides_the_email_as(String email) throws Throwable {
-		driver.findElement(By.id("user_email")).sendKeys(email);
+		driver.findElement(emailTextBoxLocator).sendKeys(email);
 	}
 
 	public void she_provides_the_password_as(String password) throws Throwable {
-		driver.findElement(By.id("user_password")).sendKeys(password);
+		driver.findElement(passwordTextBoxLocator).sendKeys(password);
 	}
 
 	public void she_provides_the_confirm_password_again_as(String confirmPassword) throws Throwable {
-		driver.findElement(By.id("user_password_confirmation")).sendKeys(confirmPassword);
+		driver.findElement(confirmPasswordTextBoxLocator).sendKeys(confirmPassword);
 	}
 
 	public void she_signs_up() throws Throwable {
-		driver.findElement(By.name("commit")).click();
+		driver.findElement(signupButtonLocator).click();
 	}
 }
