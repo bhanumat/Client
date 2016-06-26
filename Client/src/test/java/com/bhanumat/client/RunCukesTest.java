c726f29e9a601f9f719ba5aa515a638161c7864c
bhanumat <bhanumat.w@gmail.com>
2016-06-26 19:39:39 +0700

    add some formattingoptions for the results of the test run

--- Client/src/test/java/com/bhanumat/client/RunCukesTest.java
+++ Client/src/test/java/com/bhanumat/client/RunCukesTest.java
@@ -6,6 +6,10 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "classpath:com/bhanumat/client/features", glue={"com/bhanumat/client/steps"})
+@CucumberOptions(
+		features = "classpath:com/bhanumat/client/features", 
+		glue = { "com/bhanumat/client/steps" }, 
+		plugin = {"pretty", "html:target/cucumber", "json:target/cucumber-report.json" }
+)
 public class RunCukesTest {
 }
